973c23fe6731703e6650bd68d74c530bb2c740c0
Michaela White <whitemic@sas.upenn.edu>
2018-07-15 15:06:39 -0400

    Updated sidebar features

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,13 @@
 // /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
+
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    
 }
